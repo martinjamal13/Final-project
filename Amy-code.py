7cff1acb31499049715374cbebd669f554e6fea2
ah-31 <ahoang31@terpmail.umd.edu>
2020-11-08 14:04:52 -0500

    docstrings for user_input and password_hint methods

--- Amy-code.py
+++ Amy-code.py
@@ -1,1 +1,26 @@
-print("test text")
+# Amy's section of code
+
+def user_input():
+    """
+    Purpose
+        prompt user with questions to create keywords in password
+        prompt user for their username
+        prompt user for hint question and answer
+    Parameters
+    Attributes
+    Returns
+    Side Effects
+    """
+def password_hint():
+    """
+    Purpose
+        displays question and prompts user for their answer
+    Parameters
+    Attributes
+    Returns
+        incorrect input: error message
+        correct input: hint
+    Side Effects
+        incorrect input: allow 5 more tries before locking user out for n seconds
+        correct input: send user to 'password reset' function
+    """ 
